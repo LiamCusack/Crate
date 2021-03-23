6fa9ac628c33dc593eacd0fa0dd8fb6c2d922e2b
mainlyetcetera <ericcampbell294@gmail.com>
2021-03-23 16:15:30 -0600

    Find fonts

--- code/web/src/ui/common/fonts.js
+++ code/web/src/ui/common/fonts.js
@@ -1,4 +1,8 @@
 // Fonts
+// used to determine fonts, props passed to text styling
+// such as H4 and p
+// does p have a styling component?
+// I don't think so
 
 export const primary = `'Roboto', sans-serif`
 
