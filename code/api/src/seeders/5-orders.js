2388ab91a616daaceb82215055689cb42f6003de
Adam Etzion <adametzion90@gmail.com>
2021-03-30 19:30:22 -0600

    Update seeder files for all tables

--- code/api/src/seeders/5-orders.js
+++ code/api/src/seeders/5-orders.js
@@ -10,13 +10,13 @@ module.exports = {
     const subscriptionRows = subscriptions[0]
 
     return await queryInterface.bulkInsert('orders', [
-      {id: 1, deliveryDate: '3/12/21', userId: userRows[3].id, subscrptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
-      {id: 2, deliveryDate: '2/12/21', userId: userRows[3].id, subscrptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
-      {id: 3, deliveryDate: '1/12/21', userId: userRows[3].id, subscrptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
-      {id: 4, deliveryDate: '12/12/20', userId: userRows[3].id, subscrptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
-      {id: 5, deliveryDate: '3/12/21', userId: userRows[2].id, subscrptionId: subscriptionRows[2].id, createdAt: new Date(), updatedAt: new Date()},
-      {id: 6, deliveryDate: '3/12/21', userId: userRows[2].id, subscrptionId: subscriptionRows[1].id, createdAt: new Date(), updatedAt: new Date()},
-      {id: 7, deliveryDate: '4/15/21', userId: userRows[2].id, subscrptionId: subscriptionRows[2].id, createdAt: new Date(), updatedAt: new Date()}
+      {id: 1, deliveryDate: '3/12/21', subscriptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
+      {id: 2, deliveryDate: '2/12/21', subscriptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
+      {id: 3, deliveryDate: '1/12/21', subscriptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
+      {id: 4, deliveryDate: '12/12/20', subscriptionId: subscriptionRows[0].id, createdAt: new Date(), updatedAt: new Date()},
+      {id: 5, deliveryDate: '3/12/21', subscriptionId: subscriptionRows[2].id, createdAt: new Date(), updatedAt: new Date()},
+      {id: 6, deliveryDate: '3/12/21', subscriptionId: subscriptionRows[1].id, createdAt: new Date(), updatedAt: new Date()},
+      {id: 7, deliveryDate: '4/15/21', subscriptionId: subscriptionRows[2].id, createdAt: new Date(), updatedAt: new Date()}
     ], {});
   },
 
