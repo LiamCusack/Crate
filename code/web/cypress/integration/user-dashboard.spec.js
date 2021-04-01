fa31f4839be80dd4bd3634ad8b79024dc650fb2d
mainlyetcetera <ericcampbell294@gmail.com>
2021-04-01 10:32:27 -0600

    Add cypress test, user info on profile page

--- code/web/cypress/integration/user-dashboard.spec.js
+++ code/web/cypress/integration/user-dashboard.spec.js
@@ -3,15 +3,24 @@ describe('the user dashboard', () => {
     cy
       .visit('http://localhost:3000/user/login')
       .get('form div')
-      .children('input:first') 
-      .type('user@crate.com') 
+      .children('input:first')
+      .type('user@crate.com')
       .get('form div')
-      .children('div:nth-child(2)') 
-      .type('123456') 
+      .children('div:nth-child(2)')
+      .type('123456')
 
       .get('form')
       .children('div:nth-child(2)')
       .children('button')
       .click()
+
+      .get('header div div:nth-child(2) div a:nth-child(3)')
+      .click()
+
+      .get('section div div:nth-child(2) div h4')
+      .contains('The User')
+
+      .get('section div div:nth-child(2) div p')
+      .contains('user@crate.com')
   })
 });
